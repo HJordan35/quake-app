5e5d60b445d821f0599c618a3b607b0a49f9b607
H Jordan <henry@flatfile.io>
2025-04-12 08:06:32 -0500

    basic routing with Tanstack Router

---
+++ src/routes/dashboard/route.tsx
@@ -0,0 +1,21 @@
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/dashboard')({
+  component: RouteComponent,
+  beforeLoad: ({ location }) => {
+    if (location.pathname === '/dashboard') {
+      throw redirect({
+        to: '/dashboard/home',
+      })
+    }
+  },
+})
+
+function RouteComponent() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Outlet />
+    </div>
+  )
+}
