8dbc4479a8c1722160d5ef2ece21ecfac67fb0e5
H Jordan <henry@flatfile.io>
2025-04-16 22:07:04 -0500

    attempt different redirect

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,6 @@ import { createFileRoute, useNavigate } from '@tanstack/react-router'
 import { useAuth } from '@/context/AuthContext'
 import { useEffect } from 'react'
 import { Spinner } from '@/components/ui/spinner'
-
 export const Route = createFileRoute('/')({
   component: IndexComponent,
 })
@@ -12,10 +11,11 @@ function IndexComponent() {
   const navigate = useNavigate()
 
   useEffect(() => {
+    console.log('isAuthenticated', isAuthenticated)
     if (isAuthenticated && !isLoading) {
       navigate({ to: '/dashboard/home' })
     } else if (!isAuthenticated && !isLoading) {
-      navigate({ to: '/login', search: { redirect: '/' } })
+      navigate({ to: '/login', search: { redirect: '/dashboard/home' } })
     }
   }, [isAuthenticated, isLoading, navigate])
 
